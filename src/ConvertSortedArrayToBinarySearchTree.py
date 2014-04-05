f00318c47d91691b5b5605ab36768d25e264dfca
sweec <us917@yahoo.com>
2014-04-05 16:53:34 -0400

    Convert Sorted Array to Binary Search Tree

---
+++ src/ConvertSortedArrayToBinarySearchTree.py
@@ -0,0 +1,18 @@
+from Tree import TreeNode
+
+class Solution:
+    # @param num, a list of integers
+    # @return a tree node
+    def sortedArrayToBST(self, num):
+        if not num:
+            return None
+        return self.sortedArrayToBSTHelper(num, 0, len(num))
+    
+    def sortedArrayToBSTHelper(self, num, start, end):
+        index = start + (end - start)/2
+        root = TreeNode(num[index])
+        if index > start:
+            root.left = self.sortedArrayToBSTHelper(num, start, index)
+        if end > index+1:
+            root.right = self.sortedArrayToBSTHelper(num, index+1, end)
+        return root
---
+++ test/testConvertSortedArrayToBinarySearchTree.py
@@ -0,0 +1,16 @@
+import unittest
+from Tree import saveTree
+from ConvertSortedArrayToBinarySearchTree import Solution
+
+class Test(unittest.TestCase):
+
+
+    def testSortedArrayToBST(self):
+        s = Solution()
+        nums = [[1, 2, 2, 3, 3, 4, 4], range(1, 9)]
+        ans = [[3, 2, 4, 1, 2, 3, 4], [5, 3, 7, 2, 4, 6, 8, 1]]
+        for num, an in zip(nums, ans):
+            self.assertEqual(saveTree(s.sortedArrayToBST(num)), an, "result not agree")
+
+if __name__ == "__main__":
+    unittest.main()
