184e9cab41a094fa3ad1f213d6a2e008beb29731
sweec <us917@yahoo.com>
2014-04-07 14:19:14 -0400

    Unique Paths

--- src/UniquePaths.py
+++ src/UniquePaths.py
@@ -1,6 +1,8 @@
 class Solution:
     # @return an integer
     def uniquePaths(self, m, n):
+        # the number of unique paths equals
+        # the combinations number of select k from s
         s, k = m+n-2, min(m, n)-1
         num = 1
         for i in range(1, k+1):
