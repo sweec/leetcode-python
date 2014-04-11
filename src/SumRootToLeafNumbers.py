c41e41dfc95215ef15139ef3e4d26121ee8f4888
sweec <us917@yahoo.com>
2014-04-10 23:22:05 -0400

    Sum Root to Leaf Numbers

---
+++ src/SumRootToLeafNumbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def sumNumbers(self, root):
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return root.val
+        val = 0
+        if root.left:
+            root.left.val += root.val * 10
+            val += self.sumNumbers(root.left)
+            root.left.val -= root.val * 10
+        if root.right:
+            root.right.val += root.val * 10
+            val += self.sumNumbers(root.right)
+            root.right.val -= root.val * 10
+        return val
